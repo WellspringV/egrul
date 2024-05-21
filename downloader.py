a2e02072cbeb438f3b0aff5fb8892a2d15167c94
WellspringV <wellberg@list.ru>
2024-05-21 15:01:22 +0300

    mvp

---
+++ downloader.py
@@ -0,0 +1,134 @@
+import os
+import urllib
+import logging
+from pathlib import Path
+from urllib3.util.retry import Retry
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+from requests.adapters import HTTPAdapter
+
+
+
+
+logging.basicConfig(filename='mylog.log', level=logging.DEBUG)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+
+class DownloadManager:
+    def __init__(self, url: str) -> None:
+        self.url = url
+        self.filename = ""
+        self.response_headers = {}
+        self.origin_file_size = 0  
+        self.session = None     
+        self.partially_downloaded = False
+        self.ready_to_download = False
+        self._prepare_download()   
+                
+    def _get_response_headers(self) -> dict:      
+        request = urllib.request.Request(self.url, method="HEAD")
+        response = urllib.request.urlopen(request)
+        return response.headers
+        
+    def _get_origin_size(self) -> int:
+        size = int(self.response_headers.get('Content-Length', 0))
+        return size
+    
+    def _get_origin_name(self) -> str:
+        name = self.response_headers.get('filename', "")
+        if not name:
+            name = Path(urllib.parse.urlparse(self.url).path).name
+        return name
+    
+    @property
+    def local_file_exists(self) -> bool:
+        if os.path.exists(self.filename):
+            return True
+    
+    def _prepare_session(self) -> None:
+        self.session = requests.Session()
+        retry_strategy = Retry(    
+            total=3,
+            status_forcelist=[429, 500, 502, 503, 504],
+            allowed_methods=["HEAD", "GET"],
+            backoff_factor=0.5,
+            raise_on_status=False,
+            raise_on_redirect=False,
+        )         
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.session.mount("http://", adapter)
+        self.session.mount("https://", adapter)
+
+
+    def _prepare_download(self) -> None:
+        try:
+            response_headers = self._get_response_headers()
+        except Exception as ex:
+            logging.warning(ex)
+        else:
+            self.response_headers = response_headers
+            self.origin_file_size = self._get_origin_size()
+            self.filename = self._get_origin_name()
+            self._prepare_session()
+            self.ready_to_download = True
+
+    def download(self, url: str, start: int, end: int, output: str, timeout: int = 10) -> None:        
+        headers = {
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 "
+            "YaBrowser/22.11.3.838 Yowser/2.5 Safari/537.36",
+            'Range': f'bytes={start}-{end}' 
+        }
+        logging.info(f'In progress {start} of {self.origin_file_size}')
+        try:
+            rs = self.session.get(url=url, headers=headers, stream=True, timeout=timeout)
+            if 200 <= rs.status_code <= 299:                
+                with open(f'tmp/{output}', 'wb') as file:
+                    for part in rs.iter_content(1024):
+                        file.write(part)
+            else:
+                return
+        except requests.exceptions.IncompleteRead as e:
+            logging.warning(f"Ошибка чтения: {e}. Попытка повторения...")
+        except Exception as ex:
+            logging.warning(ex)
+            return
+        
+
+    def thread_run(self, thread_count: int) -> tuple:
+        file_size = self.origin_file_size
+        name = self.filename
+        chunk_size = int(file_size / thread_count)
+        chunks = range(0, file_size, chunk_size)        
+
+        with ThreadPoolExecutor(max_workers=thread_count) as executor:
+            future_list = []
+            for i, chunk in enumerate(chunks):
+                future = executor.submit(self.download, url=self.url, start=chunk, end=chunk + chunk_size - 1, output=f'{self.filename}.{i}')
+                future_list.append(future)
+
+            for f in future_list:
+                print(f.result())
+        return len(chunks), name  
+
+
+
+
+def main_download(url):
+    download_manager = DownloadManager(url)
+    chunks, name = download_manager.thread_run(10)
+
+    with open(name, "wb") as file:
+        for i in range(chunks):
+            with open(f'tmp/{Path(url).name}.{i}', 'rb') as ch:
+                file.write(ch.read())
+            Path(f'tmp/{Path(url).name}.{i}').unlink()
+
+
+if __name__ == "__main__":
+    main_download()
---
+++ f_data_base.py
@@ -0,0 +1,140 @@
+import os
+import json
+import psycopg2
+import logging
+
+from abc import ABC, abstractmethod
+
+
+logging.basicConfig(filename='mylog.log', level=logging.DEBUG)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+
+class BaseRepository(ABC):
+
+    @abstractmethod
+    def connect_to_db(self, credentials: dict) -> bool:
+        pass
+
+    @abstractmethod
+    def execute_querry(self, query: str) -> None:
+        pass
+
+    @abstractmethod
+    def insert_row(self, query: str) -> None:
+        pass
+
+    @abstractmethod
+    def select(self, query: str) -> list:
+        pass
+
+
+
+
+class PostgresRepository(BaseRepository):
+    def __init__(self):
+        self.credentials = self.get_db_credentials()
+        self.conn = self.connect_to_db(self.credentials)
+
+
+    @staticmethod
+    def get_db_credentials() -> dict:
+        db_user = os.getenv('DB_USER')
+        db_pass = os.getenv('DB_PASS')
+        db_host = os.getenv('DB_HOST', 'localhost')  
+        db_name = os.getenv('DB_NAME', 'postgres') 
+
+        if not db_user or not db_pass:
+            logging.error("Необходимы переменные окружения DB_USER и DB_PASS.")
+            exit(1)    
+        return {'user': db_user, 'password': db_pass, 'host': db_host, 'dbname': db_name}
+    
+
+    def connect_to_db(self, credentials: dict):
+        conn = None
+        try:
+            conn = psycopg2.connect(**credentials)        
+            return conn
+        except Exception as e:
+            logging.error(f"Не удалось подключиться к базе данных: {e}")
+            return None
+                
+
+    def execute_querry(self, schema: str) -> None:
+            try:
+                with self.conn.cursor() as cur:
+                    cur.execute(schema)
+                self.conn.commit()
+            except Exception as e:
+                logging.error(f"Ошибка выполнения запроса: {e}")
+            finally:
+                if self.conn:
+                    self.conn.close()
+   
+   
+    def insert_row(self, query: str) -> None:    
+        try:    
+            if self.conn.closed:  
+                self.conn = self.connect_to_db(self.credentials) 
+            with self.conn.cursor() as cur:               
+                cur.execute(query) 
+            self.conn.commit()    
+        except Exception as e:
+            logging.error(f"Ошибка при вставке данных: {e}")
+            if self.conn:
+                self.conn.rollback()
+        finally:
+            if self.conn:
+                self.conn.close()
+
+
+    def select(self, query: str) -> list:
+        try:
+            if self.conn.closed:  
+                self.conn = self.connect_to_db(self.credentials) 
+            with self.conn.cursor() as cur:
+                cur.execute(query)
+                rows = cur.fetchall()
+                return rows
+        except Exception as e:
+            logging.error(f"Ошибка при выполнении SELECT запроса: {e}")
+            return []
+        finally:
+            if self.conn:
+                self.conn.close()
+        
+
+
+
+
+
+def write_to_default_db(data):
+    repo = PostgresRepository()
+    
+    schema = """
+        CREATE TABLE IF NOT EXISTS companies (    
+            id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            data JSONB
+        );
+        """
+    if repo.connect_to_db(repo.credentials):
+        repo.execute_querry(schema)
+    else:
+        logging.error("Подключение к базе данных не удалось.")
+
+    json_string = json.dumps(data)
+    query = f"""
+        INSERT INTO companies (data)
+        VALUES ('{json_string}');
+        """ 
+    repo.insert_row(query)
+
+
+
+if __name__ == "__main__":
+    pass
